694ee711d544720c6a7b5f6d26ed6954a9846be4
José Danúbio <danubio.dm@hotmail.com>
2024-04-09 06:17:58 -0300

    test image

--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -133,11 +133,12 @@ class _ProjectsPageState extends State<ProjectsPage> {
                                       alignment: Alignment.center,
                                       children: [
                                         Image.asset(
-                                          'images/screenshots/calculadora/Screen_$_imageIndex.png',
+                                          'assets/images/screenshots/calculadora/Screen_$_imageIndex.png',
                                           width: constraints.maxWidth * 0.855,
                                           height: constraints.maxHeight * 0.855,
                                         ),
-                                        Image.asset('images/pixel_3.png'),
+                                        Image.asset(
+                                            'assets/images/pixel_3.png'),
                                       ],
                                     );
                                   },
