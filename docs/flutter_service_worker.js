bb81fba5f14b83cc0b2af14d97b559e9dee7226d
José Danúbio <danubio.dm@hotmail.com>
2024-04-11 10:53:20 -0300

    ajusts

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "9b818ca9511483c901bed1545384376c",
-"index.html": "def518649311dc793540da485fc824a0",
-"/": "def518649311dc793540da485fc824a0",
-"main.dart.js": "4f0eb4eb36015b4c0e645214fafc6312",
+"index.html": "7cadcb0970fd8fd0d76fa19f3e55211c",
+"/": "7cadcb0970fd8fd0d76fa19f3e55211c",
+"main.dart.js": "e6eec95b95e488c7de11239dcb2802ab",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double _width = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
     return SizedBox(
       height: MediaQuery.of(context).size.height,
       child: Container(
-        width: _width / 4,
+        width: width / 4,
         padding: EdgeInsets.only(
-            left: _width / 4,
-            right: _width / 4,
-            top: _width / 22,
-            bottom: _width / 20),
+            left: width / 4,
+            right: width / 4,
+            top: width / 22,
+            bottom: width / 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
               'images/profile1.png',
-              width: _width / 4,
+              width: width / 4,
             ),
             Flexible(
               child: Column(
@@ -32,29 +30,35 @@ class HomePage extends StatelessWidget {
                   Text(
                     'Danúbio Leite',
                     style: TextStyle(
-                        fontSize: _width / 45,
+                        fontSize: width / 45,
                         fontWeight: FontWeight.bold,
                         color: Color.fromRGBO(69, 117, 116, 1)),
                   ),
                   Text(
                     'Desenvolvedor Mobile',
                     style: TextStyle(
-                        fontSize: _width / 55,
+                        fontSize: width / 55,
                         fontWeight: FontWeight.bold,
                         color: Colors.black87),
                   ),
                   const SizedBox(height: 16),
                   Flexible(
                     child: RichText(
+                      textAlign: TextAlign.justify,
                       text: TextSpan(
                         style: TextStyle(
-                          fontSize: _width / 85,
+                          fontSize: width / 85,
                           color: Colors.black,
                         ),
                         children: const <TextSpan>[
                           TextSpan(
-                              text:
-                                  'Sempre em busca de novos conhecimentos, comecei a estudar Flutter três anos atrás e com ele tenho desenvolvido aplicações multiplataforma responsivas, intuitivas e com design moderno. Atualmente, estou estou trabalhando em aplicativos próprios e tenho participado de projetos como freelancer. Se você tiver alguma dúvida ou gostaria de trabalhar comigo, sinta-se à vontade para me contatar.'),
+                            text:
+                                'Há três anos venho desenvolvendo aplicações multiplataforma responsivas, intuitivas e com design moderno. Atualmente, trabalho na criação de aplicativos próprios e participo de projetos como freelancer. Precisa de ajuda para tirar sua ideia do papel? ',
+                          ),
+                          TextSpan(
+                            text: 'Fale comigo!',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
                         ],
                       ),
                     ),
